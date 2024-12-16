9dbde54d67c981b1ed5301790e8a4b91750aef35
Aidanasv <a27818@svalero.com>
2024-12-16 11:59:41 +0100

    Program y MenuApp

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Models;
+
+var menuApp = new MenuApp ();
+menuApp.MostrarMenuPrincipal();
+
